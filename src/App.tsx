d7baff3c5999b1f6ecb87d6a66939a2a089d316d
Timothée COTREL <timothee.cotrel@email.com>
2025-04-29 20:46:59 +0200

    majuscule

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Formations from "./components/formations&diplomes";
 import Competences from "./components/competences";
 import About from "./components/about";
 import Footer from "./components/footer";
-import Analytics from "./components/analytics";
+import Analytics from "./components/Analytics";
 import "./styles/App.scss";
 
 function App() {
