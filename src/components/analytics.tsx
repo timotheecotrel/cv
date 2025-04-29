08faeb4dfd217e323b77294e594b1d070a40bd08
Timothée COTREL <timothee.cotrel@email.com>
2025-04-29 14:08:31 +0200

    cookie erreur

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -16,7 +16,8 @@ const Analytics = () => {
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-XSMCLSG80J', {
-        'cookie_domain': 'auto' // Ceci permet de définir automatiquement le domaine des cookies
+        'cookie_domain': 'auto', // Ceci permet de définir automatiquement le domaine des cookies
+        'cookie_expires': 63072000  // Définir la durée de vie des cookies (exemple ici: 2 ans)
       });
     `;
     document.head.appendChild(inlineScript);
