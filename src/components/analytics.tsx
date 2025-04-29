30b103722f7a2d2d5def6a5ea4786461ce444dc3
Timothée COTREL <timothee.cotrel@email.com>
2025-04-29 21:02:41 +0200

    retour arriere

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,43 +1,26 @@
 // src/components/Analytics.tsx
 import { useEffect } from 'react';
 
-// Déclaration globale de gtag
-declare global {
-  interface Window {
-    gtag: (command: string, eventName?: string, params?: object) => void;
-    dataLayer: Array<any>;
-  }
-}
-
 const Analytics = () => {
   useEffect(() => {
-    // Injecter le script de Google Analytics
+    // Injecter le script
     const script = document.createElement('script');
     script.src = 'https://www.googletagmanager.com/gtag/js?id=G-XSMCLSG80J'; // <-- Ton ID
     script.async = true;
     document.head.appendChild(script);
 
-    // Initialiser Google Analytics une fois le script chargé
-    script.onload = () => {
-      // Initialiser dataLayer si ce n'est pas déjà fait
+    // Initialiser GA
+    const inlineScript = document.createElement('script');
+    inlineScript.innerHTML = `
       window.dataLayer = window.dataLayer || [];
-
-      // Définir la fonction gtag
-      window.gtag = function (...args: any[]) {
-        window.dataLayer.push(args);
-      };
-
-      // Configuration de Google Analytics
-      window.gtag('js', new Date().toISOString()); // Convertir Date en string ISO
-      window.gtag('config', 'G-XSMCLSG80J', {
-        cookie_domain: 'auto',  // Définit automatiquement le domaine des cookies
-        cookie_expires: 63072000,  // Durée de vie des cookies (2 ans ici)
-      });
-      console.log("Google Analytics configuré");
-    };
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-XSMCLSG80J');
+    `;
+    document.head.appendChild(inlineScript);
   }, []);
 
-  return null; // Ce composant ne rend rien, il sert uniquement à injecter le script
+  return null; // Ce composant n'affiche rien
 };
 
 export default Analytics;
